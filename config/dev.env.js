ebb1cfdb785eb515cf2bbd374ff939d6c451a9fd
brink <867547286@qq.com>
2022-01-12 18:27:46 +0800

    修改cdn

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,4 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_API: '"http://www.knifebrink.com:8080"'
+   // BASE_API: '"http://localhost:8080"'
 })
